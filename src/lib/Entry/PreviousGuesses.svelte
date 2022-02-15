eb44d3962856326a9985c02aacf23286e6d81ed3
Jacob Norwood <jacobnorwood92@gmail.com>
2022-02-15 17:32:00 -0600

    slap in some animations

--- src/lib/Entry/PreviousGuesses.svelte
+++ src/lib/Entry/PreviousGuesses.svelte
@@ -1,19 +1,23 @@
 <script lang="ts">
   import Guess from '$lib/Entry/Guess.svelte'
-  import { history, correctWord, guesses } from "../../stores/gameStore"
+  import { fade, blur, fly, slide, scale } from 'svelte/transition'
+  import { flip } from 'svelte/animate'
+  import { history, correctWord, guesses } from '../../stores/gameStore'
   $: lastCorrectWord = $history.length >= 1 ? $history[$history.length - 1].correctWord : null
 </script>
 
-
-
 {#if !!lastCorrectWord}
-  <Guess input={lastCorrectWord} correctWord={lastCorrectWord} />
+  {#each [lastCorrectWord] as correctWord, index (index)}
+    <div animate:flip in:blur out:blur>
+      <Guess input={correctWord} {correctWord} />
+    </div>
+  {/each}
 {/if}
 
-{#each $guesses as guess}
-  <Guess input={guess} correctWord={$correctWord} />
+{#each $guesses as guess, i (i)}
+  <div animate:flip in:slide out:fly>
+    <Guess input={guess} correctWord={$correctWord} />
+  </div>
 {/each}
- 
 <style lang="postcss">
 </style>
- 
--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,4 +1,4 @@
 import { localStore } from './localStore'
 
-export const theme = localStore('theme', 'dark')
+export const theme = localStore('theme', 'light')
 export const websiteUrl = localStore('websiteUrl', 'https://www.example.com')
